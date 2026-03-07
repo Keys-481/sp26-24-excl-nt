de317758cb0571f3ca55dc1e937c5e28a358ed5d
Zoe <142842810+zoelawler@users.noreply.github.com>
2026-03-06 17:40:58 -0700

    Closes #51 (#82)
    * Updated and added styling for comments and program page
    
    * Implemented a comment modal
    
    * Update package and package-lock files to include FA react icons
    
    * Fixed some styling issues
    
    * Added react fragment to return comments container and program selector
    
    * Updated to use Lucide icons

--- frontend/src/components/ProgramSelector.jsx
+++ frontend/src/components/ProgramSelector.jsx
@@ -8,7 +8,7 @@ import CommentsContainer from "./CommentComps/CommentsContainer";
 import DegreePlan from "./DegreePlanComponents/DegreePlan";
 
 // ProgramSelector component allows selection of a program for a student and displays the degree plan and comments.
-export default function ProgramSelector({ student, programs, selectedStudentProgram, setSelectedProgram, userIsStudent=false }) {
+export default function ProgramSelector({ student, programs, selectedStudentProgram, setSelectedProgram, userIsStudent = false }) {
     const [currentProgram, setCurrentProgram] = useState(selectedStudentProgram);
     const [degreePlanViewType, setDegreePlanViewType] = useState('requirements');
 
@@ -24,33 +24,33 @@ export default function ProgramSelector({ student, programs, selectedStudentProg
 
     // Render the program selector and associated components
     return (
-        <div className={`program-selector-wrapper ${userIsStudent ? 'student-layout' : 'advisor-layout'}`}>
-            {/* Program Selection List */}
-            <div className="program-selector">
-                <h3 style={{ margin: '15px' }}>Select Program:</h3>
-                {programs.length > 0 ? (
-                    <ul className="results-list">
-                        {programs.map((program, index) => (
-                            <li
-                                key={index}
-                                onClick={() => {
-                                    setSelectedProgram(program);
-                                    setCurrentProgram(program);
-                                }}
-                                className={`result-item ${currentProgram?.program_id === program.program_id ? 'selected' : ''}`}
-                            >
-                                <strong>{program.program_name}</strong>
-                                <br />
-                            </li>
-                        ))}
-                    </ul>
-                ) : (
-                    <p>No programs found for this student</p>
-                )}
-            </div>
+        <>
+            <div className={`program-selector-wrapper ${userIsStudent ? 'student-layout' : 'advisor-layout'}`}>
+                {/* Program Selection List */}
+                <div className="program-selector">
+                    <h3 style={{ margin: '15px' }}>Select Program:</h3>
+                    {programs.length > 0 ? (
+                        <ul className="results-list">
+                            {programs.map((program, index) => (
+                                <li
+                                    key={index}
+                                    onClick={() => {
+                                        setSelectedProgram(program);
+                                        setCurrentProgram(program);
+                                    }}
+                                    className={`result-item ${currentProgram?.program_id === program.program_id ? 'selected' : ''}`}
+                                >
+                                    <strong>{program.program_name}</strong>
+                                    <br />
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No programs found for this student</p>
+                    )}
+                </div>
 
-            {/* Degree Plan and Comments Section */}
-            <div className={"degree-plan-comments-wrapper"}>
+                {/* Degree Plan and Comments Section */}
                 <div className="degree-plan-wrapper">
                     {currentProgram ? (
                         <DegreePlan
@@ -63,14 +63,15 @@ export default function ProgramSelector({ student, programs, selectedStudentProg
                         <p>Select a program to view the degree plan</p>
                     )}
                 </div>
-                <CommentsContainer
-                    student={student}
-                    studentSchoolId={student.id || student.school_student_id}
-                    programId={currentProgram?.program_id}
-                    userIsStudent={userIsStudent}
-                    className={userIsStudent ? 'student-layout' : 'advisor-layout'}
-                />
+
             </div>
-        </div>
+            <CommentsContainer
+                student={student}
+                studentSchoolId={student.id || student.school_student_id}
+                programId={currentProgram?.program_id}
+                userIsStudent={userIsStudent}
+                className={userIsStudent ? 'student-layout' : 'advisor-layout'}
+            />
+        </>
     )
 }
